fd5a82a6b4aceefff03b82ea40ec76c0a4dfa276
Jakob Hoveland <83887387+Coffeejwh98@users.noreply.github.com>
2022-02-16 18:22:52 -0800

    Add files via upload

--- Client.py
+++ Client.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import *
 from tkinter import font
 from random import randint
@@ -6,6 +7,11 @@ import time
 import socket
 from time import sleep
 import threading
+import socket
+
+client = None
+HOST_ADDR = "10.156.28.126"
+HOST_PORT = 8080
 
 class Player():
     def __init__(self,name):
@@ -13,7 +19,7 @@ class Player():
         self.score = 0
 
 class Game:
-    player = Player('')
+    player = Player('Test1')
 
     def __init__(self):
         self.root = tk.Tk()
@@ -28,24 +34,41 @@ class Game:
         Label(self.welcome_frame, text='Enter name',
               bg='#DCE0E1', fg='#000000',
               font=('', 20)).pack(padx=10, pady=40)
-        entry1 = tk.Entry(self.root)
-        self.welcome_frame.create_window(200, 140, window=entry1)
+        self.entry1 = tk.Entry(self.root)
+        self.welcome_frame.create_window(200, 140, window=self.entry1)
+        self.entry1.pack(side=tk.BOTTOM)
 
         Button(self.welcome_frame, text='Submit', fg='#000000',
-               command=lambda: self.connect_screen()).pack(padx=30, pady=30)
-        self.player.name = entry1.get()
+               command=lambda: (self.set_player_name(), self.connect_screen())).pack(padx=30, pady=30)
+
+    def set_player_name(self):
+        self.player.name = self.entry1.get()
 
     def connect_screen(self):
+        self.welcome_frame.pack_forget()
+        self.entry1.destroy()
         self.connect_frame = tk.Canvas(self.root, width=400, height=300, bg='#DCE0E1')
         self.connect_frame.pack(fill=BOTH, expand=True)
-        label1 = tk.Label(self.connect_frame, text=self.player.name)
-        self.connect_frame.create_window(200, 230, window=label1)
+        self.label1 = tk.Label(self.connect_frame, text=self.player.name, bg='#DCE0E1')
+        self.label1.pack(side=tk.TOP)
+        self.connect_frame.create_window(200, 230, window=self.label1)
 
+        Button(self.connect_frame, text='Connect', fg='#000000',
+               command=lambda: (self.connect_to_server()())).pack(padx=30, pady=30)
 
-    # network client
-client = None
-HOST_ADDR = "0.0.0.0"
-HOST_PORT = 8080
+    def connect_to_server(self):
+        global client, HOST_PORT, HOST_ADDR
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect((HOST_ADDR, HOST_PORT))
+            client.send(self.player.name)
+            threading.start_new_thread(receive_ACK(), (client, "m"))
+        except Exception as e:
+            tk.messagebox.showerror(title="ERROR!!!", message="Cannot connect to host: " + HOST_ADDR + " on port: " + str(HOST_PORT) + " Server may be unavailable. Try again later")
+
+    def receive_ACK(self,sck,m):
+        from_server = sck.recv(4096)
+        print(from_server)
 
 if __name__ == "__main__":
     Game()
